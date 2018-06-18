59c84349b9c43ffbb60e571895e7c9ae93e9a5ee
Jim Wright <jimwright808@gmail.com>
2018-06-18 12:51:47 +0100

    Troubleshoot geolocation permission problem

--- localWeather/app.js
+++ localWeather/app.js
@@ -1,28 +1,23 @@
+//Add button to be pressed to activate geolocation
 
-//This needs to finish before we start the fetch. How??? Put everything in one giant function and use async/await?
-function getLocale() {
   navigator.geolocation.getCurrentPosition(function(position) {
     let latitude = (position.coords.latitude).toFixed(3);
     let longitude = (position.coords.longitude).toFixed(3);
     let url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
-    return url;
+    
+    let apiCall = function () {
+      fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.weather[0].main);
+        console.log(data.main.temp);
+        console.log(data.name, data.sys.country);
+      })
+      .catch(err => console.log('ughhhh fix it!', err));
+    }
   })
-};
-
-function apiCall () {
-  fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.weather[0].main);
-    console.log(data.main.temp);
-    console.log(data.name, data.sys.country);
-  })
-}
-
-getLocale();
-apiCall();
 
   //Use .innerText to display this Promise info on the page
 
