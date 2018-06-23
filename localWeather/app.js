8dc8c6c0f73cd1dc8c1d39ba1987f2dfa56c4b55
Jim Wright <jimwright808@gmail.com>
2018-06-23 10:08:21 +0300

    Display local weather info on page

--- localWeather/app.js
+++ localWeather/app.js
@@ -1,11 +1,13 @@
 const button = document.getElementById('locate');
+const locationDiv = document.querySelector('.location');
+const tempDiv = document.querySelector('.temp');
+const weatherDiv = document.querySelector('.weather');
 
 button.addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition(function(position) {
     let latitude = (position.coords.latitude).toFixed(3);
     let longitude = (position.coords.longitude).toFixed(3);
     let url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
-    console.log(url);
     
     (function apiCall () {
       fetch(url)
@@ -13,9 +15,14 @@ button.addEventListener('click', () => {
         return response.json();
       })
       .then((data) => {
-        console.log(data.weather[0].main);
-        console.log(data.main.temp);
-        console.log(data.name, data.sys.country);
+        locationDiv.innerText = `${data.name} ${data.sys.country}`;
+        
+        //Round number & then add Centigrade clickable element
+        tempDiv.innerText = data.main.temp;
+
+        //Do something here to display different icon depending on the weather
+        weatherDiv.innerText = data.weather[0].main;
+        
       })
       .catch(err => console.log('ughhhh fix it!', err));
     })();
