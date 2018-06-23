710ce54292882073a06188be4b788528f0030ba9
Jim Wright <jimwright808@gmail.com>
2018-06-23 16:07:34 +0300

    First attempt at getting Centigrade/Fahrenheit functionality

--- localWeather/app.js
+++ localWeather/app.js
@@ -3,6 +3,7 @@ const locationDiv = document.querySelector('.location');
 const tempDiv = document.querySelector('.temp');
 const weatherDiv = document.querySelector('.weather');
 
+
 button.addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition(function(position) {
     let latitude = (position.coords.latitude).toFixed(3);
@@ -16,7 +17,9 @@ button.addEventListener('click', () => {
       })
       .then((data) => {
         locationDiv.innerText = `${data.name} ${data.sys.country}`;
-        tempDiv.innerHTML = '<p>' + Math.round(data.main.temp) + '°<a href="">C</a></p>'
+
+        //Rather than innerHTML, I think I need to create new element and then insert into the DOM tree
+        tempDiv.innerHTML = '<p>' + Math.round(data.main.temp) + '°<a href="#" onclick="return false;">C</a></p>'
 
         //Do something here to display different icon depending on the weather
         weatherDiv.innerText = data.weather[0].main;
@@ -25,4 +28,6 @@ button.addEventListener('click', () => {
       .catch(err => console.log('ughhhh fix it!', err));
     })();
   })
+
+  document.querySelector("a[href='#']").addEventListener('click', () => console.log('it worked'));
 })
