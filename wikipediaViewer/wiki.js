f214fef492d4f68d68cceee11e8c5a0e3c101da0
Jim Wright <jimwright808@gmail.com>
2018-07-01 12:18:50 +0100

    Event Listener for search box and save value in variable for API call

--- wikipediaViewer/wiki.js
+++ wikipediaViewer/wiki.js
@@ -1,6 +1,14 @@
 const randomText = document.querySelector('#random');
 const randomUrl = 'https://en.wikipedia.org/wiki/Special:Random';
+const searchBox = document.querySelector('#searchBox');
 
 randomText.addEventListener('click', () => {
     window.open(randomUrl);
+});
+
+searchBox.addEventListener('keypress', (event) => {
+    if (event.which === 13) {
+        let searchText = searchBox.value;
+        console.log(searchText);
+    }
 });
