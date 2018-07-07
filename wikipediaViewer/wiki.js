010db7324993689feb826dbbff9884235d664f54
Jim Wright <jimwright808@gmail.com>
2018-07-07 13:18:37 +0100

    Add comments to JS file to aid understanding

--- wikipediaViewer/wiki.js
+++ wikipediaViewer/wiki.js
@@ -2,15 +2,21 @@ const randomText = document.querySelector('#random');
 const randomUrl = 'https://en.wikipedia.org/wiki/Special:Random';
 const searchBox = document.querySelector('#searchBox');
 
+//Random Wiki article feature
 randomText.addEventListener('click', () => {
     window.open(randomUrl);
 });
 
+//Listen for Enter keypress in search box
 searchBox.addEventListener('keypress', (event) => {
     if (event.which === 13) {
+        //Save inputted text in variable
         let searchText = searchBox.value;
+
+        //Compile Wiki page url
         let searchUrl = `https://en.wikipedia.org/w/api.php?action=query&titles=${searchText}&format=jsonfm&formatversion=2`;
         
+        //Fetch Wiki API data and convert to JSON
         fetch(searchUrl, {mode: 'no-cors'})
         .then(((response) => {
             return response.json();
