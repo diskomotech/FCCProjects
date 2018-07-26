cdb06a7989ce46e3b50304766f1be8e3e1ce7ac3
Jim Wright <jimwright808@gmail.com>
2018-07-26 12:39:34 +0100

    Solve basic parsing issue

--- markdownPreviewer/markdown.js
+++ markdownPreviewer/markdown.js
@@ -2,7 +2,7 @@ const editor = document.querySelector('#editor');
 const previewer = document.querySelector('#preview');
 
 //Placeholder text displaying in previewer when page loads
-previewer.innerHTML = editor.value
+previewer.innerHTML = marked(editor.value);
 
 //Listen for keyboard changes and update previewer window
-editor.addEventListener('input', () => previewer.innerHTML = editor.value);
+editor.addEventListener('input', () => previewer.innerHTML = marked(editor.value));
