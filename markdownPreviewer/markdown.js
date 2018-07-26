a107b57f7252103e7ffe5d69dad73b3e8bb107eb
Jim Wright <jimwright808@gmail.com>
2018-07-26 16:49:25 +0100

    Small progress with markdown parsing

--- markdownPreviewer/markdown.js
+++ markdownPreviewer/markdown.js
@@ -1,10 +1,14 @@
 const editor = document.querySelector('#editor');
 const previewer = document.querySelector('#preview');
 
+const markedUp = '# Marked in the browser\n\nRendered by **markedddddd**.'
+// let myMarkUp = editor.value;
+let myMarkUp = "# Welcome to my Markdown Previewer! \n## This is a sub-heading... \n### And here's some other cool stuff";
+
 //Placeholder text displaying in previewer when page loads
 
 // previewer.innerHTML = marked(`${editor.value}`);
-previewer.innerHTML = marked('# Marked in the browser\n\nRendered by **marked**.');
+previewer.innerHTML = marked(myMarkUp);
 
 //Listen for keyboard changes and update previewer window
 // editor.addEventListener('input', () => previewer.innerHTML = marked(editor.value));
