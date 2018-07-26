e0f91b1e0efbce50b1fd2e1c2ce43f6dbbe6e553
Jim Wright <jimwright808@gmail.com>
2018-07-26 12:53:20 +0100

    Progress on making parsing work properly

--- markdownPreviewer/markdown.js
+++ markdownPreviewer/markdown.js
@@ -2,7 +2,9 @@ const editor = document.querySelector('#editor');
 const previewer = document.querySelector('#preview');
 
 //Placeholder text displaying in previewer when page loads
-previewer.innerHTML = marked(editor.value);
+
+// previewer.innerHTML = marked(`${editor.value}`);
+previewer.innerHTML = marked('# Marked in the browser\n\nRendered by **marked**.');
 
 //Listen for keyboard changes and update previewer window
-editor.addEventListener('input', () => previewer.innerHTML = marked(editor.value));
+// editor.addEventListener('input', () => previewer.innerHTML = marked(editor.value));
