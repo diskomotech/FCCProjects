384a527b1f957ab3a7eb69b4949c491312415c46
Jim Wright <jimwright808@gmail.com>
2018-07-27 12:47:39 +0100

    Use regular expression to deal with multi-line string input

--- markdownPreviewer/markdown.js
+++ markdownPreviewer/markdown.js
@@ -1,14 +1,35 @@
 const editor = document.querySelector('#editor');
 const previewer = document.querySelector('#preview');
 
-const markedUp = '# Marked in the browser\n\nRendered by **markedddddd**.'
-// let myMarkUp = editor.value;
 let myMarkUp = "# Welcome to my Markdown Previewer! \n## This is a sub-heading... \n### And here's some other cool stuff";
 
-//Placeholder text displaying in previewer when page loads
+const text = editor.value;
+const match = /\r|\n/.exec(text);
+if (match) {
+    console.log('Found one');
+}
 
+// previewer.innerHTML = marked(editor.value);
+
+// const h1 = '# This is an h1';
+// const h2 = '## This is an h2';
+// const link = '[links](https://www.freecodecamp.com)';
+// const inLineCode = 'Heres some code, `<div></div>`, between 2 backticks.'
+// // const codeBlock = "" ```
+// //     function anotherExample(firstLine, lastLine) {
+// //         if (firstLine == '```' && lastLine == '```') {
+// //             return multiLineCode;
+// //         }   
+// //     }
+// // ``` ;
+// const list = '- And of course there are lists. - Some are bulleted.  - With different indentation levels. - That look like this.'
+// const blockQuote = '> Block Quotes!';
+// const bold = 'text **bold**';
+// const image = '![React Logo w/ Text](https://goo.gl/Umyytc)';
+
+//Placeholder text displaying in previewer when page loads
 // previewer.innerHTML = marked(`${editor.value}`);
-previewer.innerHTML = marked(myMarkUp);
+// previewer.innerHTML = marked(editor.value);
 
 //Listen for keyboard changes and update previewer window
 // editor.addEventListener('input', () => previewer.innerHTML = marked(editor.value));
