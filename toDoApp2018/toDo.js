d259d404d4faf6bddb2b745a0ad006a0d2d2a5e4
Jim Wright <jimwright808@gmail.com>
2018-07-29 12:32:16 +0100

    Stuck with arrays and element selecting

---
+++ toDoApp2018/toDo.js
@@ -0,0 +1,16 @@
+const li = document.getElementsByTagName('li')[0];
+const textInput = document.querySelector("#textInput");
+
+// items.addEventListener("click", (element) => {
+//     element.classList.toggle("completed")
+// });
+
+li.addEventListener("click", (event) => {
+    console.log("Working");
+})
+
+// li.forEach(() => {
+//     ul[i].addEventListener("click", (event) => {
+//         console.log("Working");
+//     })
+// });
