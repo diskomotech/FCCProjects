3277d6ab0105be5b125268b09c37e84571dcf33a
Jim Wright <jimwright808@gmail.com>
2018-08-02 19:34:38 +0100

    Finally solved append new item problem

--- toDoApp2018/toDo.js
+++ toDoApp2018/toDo.js
@@ -3,6 +3,7 @@ const textInput = document.querySelector("#textInput");
 let newToDoItem = "";
 
 
+
 ul.addEventListener("click", (event) => {
     if (event.target.tagName == "LI") {
         event.target.classList.toggle("completed");
@@ -13,6 +14,9 @@ textInput.addEventListener("keypress", (event) => {
     if (event.which === 13) {
        newToDoItem = textInput.value;
        //Need to find a way to target last node and then add new node after it
+       let li = document.createElement("li");
+
+       ul.appendChild(li).append(newToDoItem);
 
     //    let arr = [].slice.call(ul);
     //    let lastNode = ul.length;
