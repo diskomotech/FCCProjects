091a9e3c13ae368932306fba6d2b7f26fca34c00
Jim Wright <jimwright808@gmail.com>
2018-08-03 20:19:44 +0100

    Tidy up code

--- toDoApp2018/toDo.js
+++ toDoApp2018/toDo.js
@@ -2,8 +2,6 @@ const ul = document.querySelector('ul');
 const textInput = document.querySelector("#textInput");
 let newToDoItem = "";
 
-
-
 ul.addEventListener("click", (event) => {
     if (event.target.tagName == "LI") {
         event.target.classList.toggle("completed");
@@ -13,12 +11,9 @@ ul.addEventListener("click", (event) => {
 textInput.addEventListener("keypress", (event) => {
     if (event.which === 13) {
        newToDoItem = textInput.value;
-       //Need to find a way to target last node and then add new node after it
        let li = document.createElement("li");
 
        ul.appendChild(li).append(newToDoItem);
 
-    //    let arr = [].slice.call(ul);
-    //    let lastNode = ul.length;
     }
 });
