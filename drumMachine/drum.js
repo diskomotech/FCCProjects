a85afe4cf702dea4015b934274e1d53f89b53436
Jim Wright <jimwright808@gmail.com>
2018-08-05 14:44:27 +0100

    Remove howler.js. Add basic HTML5 audio code

--- drumMachine/drum.js
+++ drumMachine/drum.js
@@ -1,5 +1,29 @@
 const qButton = document.querySelector("#Q");
+//Rest of keyboard buttons to go here
+
+
+
+
+
+const context = new window.AudioContext();
+
+function playFile(filepath) {
+  // Fetch the file
+  fetch(filepath)
+    // Read it into memory as an arrayBuffer
+    .then(response => response.arrayBuffer())
+    // Turn it from mp3/aac/whatever into raw audio data
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => {
+      // Now we're ready to play!
+      const soundSource = context.createBufferSource();
+      soundSource.buffer = audioBuffer;
+      soundSource.connect(context.destination);
+      soundSource.start();
+    });
+}
 
 qButton.addEventListener('click', () => {
     console.log("Working");
+    playFile('https://s3.eu-west-2.amazonaws.com/diskomotech/freeCodeCamp/drumMachine/sounds/clap.mp3');
 });
