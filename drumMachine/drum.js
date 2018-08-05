155f895d5a1dcdc4b59eafff465cdca515221195
Jim Wright <jimwright808@gmail.com>
2018-08-05 15:28:26 +0100

    Add sound trigger functionality in a separate function to keep code DRY

--- drumMachine/drum.js
+++ drumMachine/drum.js
@@ -1,9 +1,18 @@
 const qButton = document.querySelector("#Q");
-//Rest of keyboard buttons to go here
+const wButton = document.querySelector("#W");
+const eButton = document.querySelector("#E");
+const aButton = document.querySelector("#A");
+const sButton = document.querySelector("#S");
+const dButton = document.querySelector("#D");
+const zButton = document.querySelector("#Z");
+const xButton = document.querySelector("#X");
+const cButton = document.querySelector("#C");
 
-
+const playIt = function (url) {
+    const player = new SoundPlayer();
+    player.load(url).play();
+}
 
 qButton.addEventListener('click', () => {
-    const player = new SoundPlayer();
-    player.load('https://s3.eu-west-2.amazonaws.com/diskomotech/freeCodeCamp/drumMachine/sounds/clap.mp3').play();
+    playIt('https://s3.eu-west-2.amazonaws.com/diskomotech/freeCodeCamp/drumMachine/sounds/clap.mp3');
 });
