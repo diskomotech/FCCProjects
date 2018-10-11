28b4115d452499c4077ba69fdc09f675d5200ebf
Jim Wright <jimwright808@gmail.com>
2018-10-11 10:12:17 +0400

    Basic AddEventListeners set-up

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -168,3 +168,32 @@
 // });
 
 
+
+
+//We can use an event delegation pattern to listen, since keys are all children of .calculator__keys.
+
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+
+keys.addEventListener('click', e => {
+    if (e.target.classList.contains('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+
+        if (!action) {
+            console.log('number key!');
+          }          
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            console.log('operator key!');
+        }
+          if (action === 'decimal') {
+            console.log('decimal key!');
+          }
+          if (action === 'clear') {
+            console.log('clear key!');
+          }
+          if (action === 'calculate') {
+            console.log('equal key!');
+          }
+    }
+});
