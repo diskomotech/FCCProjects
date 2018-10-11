391b02ea2656aee965f9ad3dcc0bf158332817f0
Jim Wright <jimwright808@gmail.com>
2018-10-11 09:36:43 +0400

    Add data-action attributes to HTML buttons

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,186 +1,170 @@
-const clear = document.querySelector("#clear");
-const divide = document.querySelector("#divide");
-const multiply = document.querySelector("#multiply");
-const subtract = document.querySelector("#subtract");
-const add = document.querySelector("#add");
-const equals = document.querySelector("#equals");
-const decimal = document.querySelector("#decimal");
-const zero = document.querySelector("#zero");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const display = document.querySelector("#display");
-const decimalRegex = /\.[^.]*$/;
-const operatorRegex = /[+-/*]/g;
-let input = "";
-let output;
+// const clear = document.querySelector("#clear");
+// const divide = document.querySelector("#divide");
+// const multiply = document.querySelector("#multiply");
+// const subtract = document.querySelector("#subtract");
+// const add = document.querySelector("#add");
+// const equals = document.querySelector("#equals");
+// const decimal = document.querySelector("#decimal");
+// const zero = document.querySelector("#zero");
+// const one = document.querySelector("#one");
+// const two = document.querySelector("#two");
+// const three = document.querySelector("#three");
+// const four = document.querySelector("#four");
+// const five = document.querySelector("#five");
+// const six = document.querySelector("#six");
+// const seven = document.querySelector("#seven");
+// const eight = document.querySelector("#eight");
+// const nine = document.querySelector("#nine");
+// const display = document.querySelector("#display");
+// const decimalRegex = /\.[^.]*$/;
+// const operatorRegex = /[+-/*]/g;
+// let input = "";
+// let output;
 
-function displayStuff () {
-    display.innerHTML = input;
-}
+// function displayStuff () {
+//     display.innerHTML = input;
+// }
 
-function roundAnswer(x) {
-    if (x % 1 != 0) {
-        return Number.parseFloat(x).toFixed(5);
-    }
-    else {
-        return x;
-    }
-  }
+// function roundAnswer(x) {
+//     if (x % 1 != 0) {
+//         return Number.parseFloat(x).toFixed(5);
+//     }
+//     else {
+//         return x;
+//     }
+//   }
 
-function consecutiveOperators(inputted, operator) {
-    if (inputted.slice(-2).match(operatorRegex) !== null) {  
-        input = inputted.slice(0, inputted.length-1) + inputted.slice(-1);
-    }
-    else {
-      input = input + operator;
-    }
-}
+// function consecutiveOperators(inputted, operator) {
+//     if (inputted.slice(-2).match(operatorRegex) !== null) {  
+//         input = inputted.slice(0, inputted.length-1) + inputted.slice(-1);
+//     }
+//     else {
+//       input = input + operator;
+//     }
+// }
 
-//NOT COMPLETED!! Main issue is still how to search for any of +-/* and not just one at a time but this is a good start
-function decimalSearch(operator) {
-    let tempInput = input;
-    let i = tempInput.lastIndexOf(operator);
+// //NOT COMPLETED!! Main issue is still how to search for any of +-/* and not just one at a time but this is a good start
+// function decimalSearch(operator) {
+//     let tempInput = input;
+//     let i = tempInput.lastIndexOf(operator);
 
-    if (i !== -1) {
-      temptInput = tempInput.slice(i+1);
-      if (tempInput.match(decimalRegex)) {
-        return;
-    }
-    else {
-      input = input + operator;
-    }
-  }
-} 
+//     if (i !== -1) {
+//       temptInput = tempInput.slice(i+1);
+//       if (tempInput.match(decimalRegex)) {
+//         return;
+//     }
+//     else {
+//       input = input + operator;
+//     }
+//   }
+// } 
 
-one.addEventListener("click", () => {
-    input = input + "1";
-    displayStuff();
-});
+// one.addEventListener("click", () => {
+//     input = input + "1";
+//     displayStuff();
+// });
 
-two.addEventListener("click", () => {
-    input = input + "2";
-    displayStuff();
-});
+// two.addEventListener("click", () => {
+//     input = input + "2";
+//     displayStuff();
+// });
 
-three.addEventListener("click", () => {
-    input = input + "3";
-    displayStuff();
-});
+// three.addEventListener("click", () => {
+//     input = input + "3";
+//     displayStuff();
+// });
 
-four.addEventListener("click", () => {
-    input = input + "4";
-    displayStuff();
-});
+// four.addEventListener("click", () => {
+//     input = input + "4";
+//     displayStuff();
+// });
 
-five.addEventListener("click", () => {
-    input = input + "5";
-    displayStuff();
-});
+// five.addEventListener("click", () => {
+//     input = input + "5";
+//     displayStuff();
+// });
 
-six.addEventListener("click", () => {
-    input = input + "6";
-    displayStuff();
-});
+// six.addEventListener("click", () => {
+//     input = input + "6";
+//     displayStuff();
+// });
 
-seven.addEventListener("click", () => {
-    input = input + "7";
-    displayStuff();
-});
+// seven.addEventListener("click", () => {
+//     input = input + "7";
+//     displayStuff();
+// });
 
-eight.addEventListener("click", () => {
-    input = input + "8";
-    displayStuff();
-});
+// eight.addEventListener("click", () => {
+//     input = input + "8";
+//     displayStuff();
+// });
 
-nine.addEventListener("click", () => {
-    input = input + "9";
-    displayStuff();
-});
+// nine.addEventListener("click", () => {
+//     input = input + "9";
+//     displayStuff();
+// });
 
-zero.addEventListener("click", () => {
-    //This needs fixing. Currently can't enter '100'
-    let last = input.split("").pop();
-    if (last !== "0" || input.match(decimalRegex) !== null) {
-        input = input + "0";
-        displayStuff();
-    }
-});
+// zero.addEventListener("click", () => {
+//     //This needs fixing. Currently can't enter '100'
+//     let last = input.split("").pop();
+//     if (last !== "0" || input.match(decimalRegex) !== null) {
+//         input = input + "0";
+//         displayStuff();
+//     }
+// });
 
-add.addEventListener("click", () => {
-    consecutiveOperators(input, "+");
-    displayStuff();
-});
+// add.addEventListener("click", () => {
+//     consecutiveOperators(input, "+");
+//     displayStuff();
+// });
 
-subtract.addEventListener("click", () => {
-    consecutiveOperators(input, "-");
-    displayStuff();
-});
+// subtract.addEventListener("click", () => {
+//     consecutiveOperators(input, "-");
+//     displayStuff();
+// });
 
-multiply.addEventListener("click", () => {
-    consecutiveOperators(input, "*");
-    displayStuff();
-});
+// multiply.addEventListener("click", () => {
+//     consecutiveOperators(input, "*");
+//     displayStuff();
+// });
 
-divide.addEventListener("click", () => {
-    consecutiveOperators(input, "/");
-    displayStuff();
-});
+// divide.addEventListener("click", () => {
+//     consecutiveOperators(input, "/");
+//     displayStuff();
+// });
 
-decimal.addEventListener("click", () => {
-    let last = input.slice(-1);
+// decimal.addEventListener("click", () => {
+//     let last = input.slice(-1);
     
-    //If last digit entered was a decimal, stop user entering another decimal straight away
-    if (last === ".") {
-        return;
-    }
+//     //If last digit entered was a decimal, stop user entering another decimal straight away
+//     if (last === ".") {
+//         return;
+//     }
 
-    //Stop user from entering more than decimal on any side of an operator
-    //To do this, search input array for an operator
-    //If (operator is not there) {
-        //Do we already have a decimal? Yes? Then, prevent another being entered. No? Allow decimal to be entered.
-    // }
-    // Else if (operator is there) {
-        //Do we already have a decimal to the right of the operator? Yes? Then, prevent another being entered. No? Allow decimal to be entered.
-   // }
+//     //This largely works but only solves half of the problem. 
+//     if (input.match(operatorRegex) === null) {
+//         if (input.match(decimalRegex) !== null) {
+//             return;
+//         }
+//         else {
+//             input = input + ".";
+//             displayStuff();
+//         }
+//     }
+//     else {
+//         input = input + ".";
+//         displayStuff();
+//     }
+// });
 
-    //This doesn't work. Struggling to make it work right now. Parking the issue for the time being.
-     //   if (input.match(operatorRegex) !== null) {
-    //I want to break input and just look at the right hand side of the operator
-    // let splitInput = input.split("+");   
-    // let x = input.indexOf(operatorRegex);
-    //     let rightHandSearch = decimalSearch.slice(x);
-    //     console.log(x);
-    // }
+// equals.addEventListener("click", () => {
+//     input = roundAnswer(mathFromString(input));
+//     displayStuff();
+// });
 
-    //This largely works but only solves half of the problem. 
-    if (input.match(operatorRegex) === null) {
-        if (input.match(decimalRegex) !== null) {
-            return;
-        }
-        else {
-            input = input + ".";
-            displayStuff();
-        }
-    }
-    else {
-        input = input + ".";
-        displayStuff();
-    }
-});
+// clear.addEventListener("click", () => {
+//     input = "";
+//     display.innerHTML = 0;
+// });
 
-equals.addEventListener("click", () => {
-    input = roundAnswer(mathFromString(input));
-    displayStuff();
-});
 
-clear.addEventListener("click", () => {
-    input = "";
-    display.innerHTML = 0;
-});
