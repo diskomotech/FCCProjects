b0e0f48d1ae8d7372f220cf7060d56cead918f0b
Jim Wright <jimwright808@gmail.com>
2018-10-12 13:06:06 +0400

    Refactor calculate function

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -185,19 +185,13 @@ keys.addEventListener('click', e => {
         let previousKeyType = calculator.dataset.previousKeyType;
         
         const calculate = (n1, operator, n2) => {
-            let result = '';
-            if (operator === 'add') {
-                result = parseFloat(parseFloat(n1)) + parseFloat(n2);
-            } else if (operator === 'subtract') {
-                result = parseFloat(n1) - parseFloat(n2);
-            } else if (operator === 'multiply') {
-                result = parseFloat(n1) * parseFloat(n2);
-            } else if (operator === 'divide') {
-                result = parseFloat(n1) / parseFloat(n2);
-            }
-
-            return result;
-        };
+            const firstNum = parseFloat(n1);
+            const secondNum = parseFloat(n2);
+            if (operator === 'add') return firstNum + secondNum;
+            if (operator === 'subtract') return firstNum - secondNum;
+            if (operator === 'multiply') return firstNum * secondNum;
+            if (operator === 'divide') return firstNum / secondNum;
+          };
 
         if (!action) {
             if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
