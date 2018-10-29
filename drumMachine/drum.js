3e8f166f5e0065c41ce74ee47812ae2001f8eafb
Jim Wright <jimwright808@gmail.com>
2018-10-29 12:11:44 +0400

    Fix problem with removing visual keypress effect

--- drumMachine/drum.js
+++ drumMachine/drum.js
@@ -1,21 +1,30 @@
-const drumMachine = document.querySelector('#drum-machine');
 const display = document.querySelector('.display');
 const keys = document.querySelector('.drum-pad');
 
 keys.addEventListener('click', (event) => {
     if (event.target.className === 'drum-pad__button') {
+        // Var to save typing later
         const action = event.target.dataset.action;
+        const key = event.target;
+
+        //Initiate new instance of external sound player
         const player = new SoundPlayer();
+
+        //Generate AWS URL for our sounds
         const urlGenerator = (() => `https://s3.eu-west-2.amazonaws.com/diskomotech/freeCodeCamp/drumMachine/sounds/${action}.mp3`)();
-        player.load(urlGenerator).play();    
+        
+        //Play the sound
+        player.load(urlGenerator).play(); 
+
+        // Display sound name 
         display.textContent = action;
+        
+        // Add press effect animation
+        key.classList.add('triggered');
+
+        // Remove press effect animation
+        setTimeout(() => {
+            key.classList.remove('triggered');
+        }, 80);
     }   
 });
-
-//Remove button press effect once transition ended
-function removeTransition (event) {
-    if (event.propertyName !== "transform") return;
-    this.classList.remove("triggered");
-}
-
-Array.from(keys.children).forEach(key => key.addEventListener("transitionend", removeTransition));
