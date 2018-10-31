4c300c570dd88082bfbb257966b957f4923ee3b4
Jim Wright <jimwright808@gmail.com>
2018-10-31 11:52:13 +0400

    Add more comments

--- wikipediaViewer/wiki.js
+++ wikipediaViewer/wiki.js
@@ -20,23 +20,24 @@ magnifyIcon.addEventListener('click', () => {
 //Listen for Enter keypress in search box
 searchBox.addEventListener('keypress', (event) => {
     if (event.which === 13) {
-        //Save inputted text in variable
-        let searchText = searchBox.value;
+            //Save inputted text in variable
+            let searchText = searchBox.value;
 
-        //Compile Wiki page url
-        let searchUrl = `https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=${searchText}&limit=8`
-        
-        //Fetch Wiki API data and convert to JSON
-        fetch(searchUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            for (let i = 0; i < data[1].length; i++) {
-                html += `<div class = 'wikiEntries transitionFX'>`;
-                html += `<a href="${data[3][i]}" target="_blank">${data[1][i]}</a>`
-                html += `<p>${data[2][i]}</p></div>`; 
-                resultsBox.innerHTML = html;
-            }
-        })
-    html = '';
+            //Compile Wiki page url
+            let searchUrl = `https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=${searchText}&limit=8`
+            
+            //Fetch Wiki API data and convert to JSON
+            fetch(searchUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                for (let i = 0; i < data[1].length; i++) {
+                    html += `<div class = 'wikiEntries transitionFX'>`;
+                    html += `<a href="${data[3][i]}" target="_blank">${data[1][i]}</a>`
+                    html += `<p>${data[2][i]}</p></div>`; 
+                    resultsBox.innerHTML = html;
+                }
+            })
+        // Reset html variable so new search results aren't tacked onto the end of previous search results    
+        html = '';
     }
 });
