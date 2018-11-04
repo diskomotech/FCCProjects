280c8732f220bd899ffc262625a818d1f593bc1d
Jim Wright <jimwright808@gmail.com>
2018-11-04 11:45:24 +0400

    Fix spacing issue

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,20 +10,22 @@ keys.addEventListener('click', e => {
         //No dataset attribute so must be number key
         if (!action) {
             console.log('number key!');
-          }
+        }
 
           if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
             console.log('operator key!');
-          }
+        }
 
           if (action === 'decimal') {
             console.log('decimal key!');
-          }
+        }
+
           if (action === 'clear') {
             console.log('clear key!');
-          }
+        }
+
           if (action === 'calculate') {
             console.log('equal key!');
-          }
+        }
     }
-   })
+})
