4dc5d08c2676ad0afa22aabb3d8a8f6ff8f34482
Jim Wright <jimwright808@gmail.com>
2018-11-04 15:45:24 +0400

    Tidy up JS formatting style

--- toDoApp2018/toDo.js
+++ toDoApp2018/toDo.js
@@ -1,37 +1,37 @@
-const ul = document.querySelector("ul");
+const ul = document.querySelector('ul');
 
 //Mark item as completed
-ul.addEventListener("click", (event) => {
-    if (event.target.tagName == "LI") {
-        event.target.classList.toggle("completed");
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle('completed');
     }
 });
 
 //Create new to-do list items
-textInput.addEventListener("keypress", (event) => {
-    if (event.which === 13) {
+textInput.addEventListener('keypress', (e) => {
+    if (e.which === 13) {
         //Create new DOM elements to be added to UL
-       const li = document.createElement("li");
-       const newSpan = document.createElement("span");
-       const textInput = document.querySelector("#textInput");
+       const li = document.createElement('li');
+       const newSpan = document.createElement('span');
+       const textInput = document.querySelector('#textInput');
 
         //Save text input in var
        const newToDoItem = textInput.value;
 
        //Add (temporary) X delete button to span
-       newSpan.innerHTML = '<i class="fa fa-trash"></i>';
+       newSpan.innerHTML = `<i class='fa fa-trash'></i>`;
 
        //Append the LI to UL and add text input
        ul.appendChild(li).append(newSpan, newToDoItem);
 
        //Clear text input box once everything is done
-       textInput.value = "";
+       textInput.value = '';
     }
 });
 
 //Delete list item when trashcan icon clicked
-ul.addEventListener("click", (event) => {
-    if (event.target.tagName == "I") {
-        event.target.parentNode.parentNode.remove();
+ul.addEventListener('click', (e) => {
+    if (e.target.tagName == 'I') {
+        e.target.parentNode.parentNode.remove();
     }
 });
