53d5531f2f59729d8da3ffb067b815bc35913c2e
Jim Wright <jimwright808@gmail.com>
2018-11-11 13:07:58 +0400

    Got basic timer and displayTime functions set-up

--- pomodoroClock/app.js
+++ pomodoroClock/app.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelectorAll('button');
+const timerDisplay = document.querySelector('.display__time-left');
+let countdown;
 
 buttons.forEach(button => button.addEventListener('click', buttonClicked));
 
@@ -23,3 +25,27 @@ function buttonClicked () {
         console.log(dataAttribute);
     }
 }
+
+function timer (seconds) {
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0) {
+          clearInterval(countdown);
+          return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+  }
