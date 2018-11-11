c3b65deca4ba8190da67ea5c3f9c5263ef7b6c34
Jim Wright <jimwright808@gmail.com>
2018-11-11 12:56:00 +0400

    Add event listeners for button clicks

--- pomodoroClock/app.js
+++ pomodoroClock/app.js
@@ -3,5 +3,23 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', buttonClicked));
 
 function buttonClicked () {
-    console.log(this.dataset.action);
+    const dataAttribute = this.dataset.action;
+    if (dataAttribute === 'session-increment') {
+        console.log(dataAttribute);
+    }
+    if (dataAttribute === 'session-decrement') {
+        console.log(dataAttribute);
+    }
+    if (dataAttribute === 'break-increment') {
+        console.log(dataAttribute);
+    }
+    if (dataAttribute === 'break-decrement') {
+        console.log(dataAttribute);
+    }
+    if (dataAttribute === 'start-stop') {
+        console.log(dataAttribute);
+    }
+    if (dataAttribute === 'reset') {
+        console.log(dataAttribute);
+    }
 }
