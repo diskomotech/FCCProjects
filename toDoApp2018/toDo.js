7cbd98c577ba66c4bfde2f1a9cca1671cbc2a110
Jim Wright <jimwright808@gmail.com>
2018-11-15 11:49:24 +0400

    Run ESLint

--- toDoApp2018/toDo.js
+++ toDoApp2018/toDo.js
@@ -1,34 +1,34 @@
 const ul = document.querySelector('ul');
 
-//Mark item as completed
+// Mark item as completed
 ul.addEventListener('click', (e) => {
-    if (e.target.tagName == 'LI') {
-        e.target.classList.toggle('completed');
-    }
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('completed');
+  }
 });
 
-//Create new to-do list items
+// Create new to-do list items
 textInput.addEventListener('keypress', (e) => {
-    if (e.which === 13) {
-        //Create new DOM elements to be added to UL
-       const li = document.createElement('li');
-       const newSpan = document.createElement('span');
-       const newToDoItem = (document.querySelector('#textInput')).value;
+  if (e.which === 13) {
+    // Create new DOM elements to be added to UL
+    const li = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newToDoItem = (document.querySelector('#textInput')).value;
 
-       //Add (temporary) X delete button to span
-       newSpan.innerHTML = `<i class='fa fa-trash'></i>`;
+    // Add (temporary) X delete button to span
+    newSpan.innerHTML = '<i class=\'fa fa-trash\'></i>';
 
-       //Append the LI to UL and add text input
-       ul.appendChild(li).append(newSpan, newToDoItem);
+    // Append the LI to UL and add text input
+    ul.appendChild(li).append(newSpan, newToDoItem);
 
-       //Clear text input box once everything is done
-       textInput.value = '';
-    }
+    // Clear text input box once everything is done
+    textInput.value = '';
+  }
 });
 
-//Delete list item when trashcan icon clicked
+// Delete list item when trashcan icon clicked
 ul.addEventListener('click', (e) => {
-    if (e.target.tagName == 'I') {
-        e.target.parentNode.parentNode.remove();
-    }
+  if (e.target.tagName == 'I') {
+    e.target.parentNode.parentNode.remove();
+  }
 });
