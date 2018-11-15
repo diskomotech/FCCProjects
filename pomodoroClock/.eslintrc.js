f45d9505aedbfdc5efab8de6d44c8f08786452c1
Jim Wright <jimwright808@gmail.com>
2018-11-15 11:24:02 +0400

    Update ESLint rules and settings

--- pomodoroClock/.eslintrc.js
+++ pomodoroClock/.eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "es6": true,
+        "browser": true
+      },
+    "rules": {
+        "no-console": 1,
+        "no-unused-vars": 1,
+        "no-use-before-define": 0
+      }
 };
