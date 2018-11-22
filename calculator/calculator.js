597dfbe7a034d499ecd44c4161f61da7d03284fa
Jim Wright <jimwright808@gmail.com>
2018-11-22 14:49:49 +0400

    Got number display working

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,15 +1,22 @@
 const calculator = document.querySelector('.calculator');
 const display = calculator.querySelector('.calculator__display');
 const keys = calculator.querySelector('.calculator__keys');
+let inputted;
 
 keys.addEventListener('click', (e) => {
   if (e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
 
     // No dataset attribute so must be number key
     if (!action) {
-      displayInput(key.textContent);
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
 
     if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
@@ -29,7 +36,3 @@ keys.addEventListener('click', (e) => {
     }
   }
 });
-
-function displayInput(keyPressed) {
-  display.textContent = keyPressed;
-}
