3be4a05b5dab7911c4cde6449ad7b49968719ced
Jim Wright <jimwright808@gmail.com>
2018-12-18 10:34:49 +0400

    Initial set-up of files

---
+++ randomQuote/react-version/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import Quotes from "./Quotes";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello world</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ randomQuote/react-version/src/Quotes.js
@@ -0,0 +1,61 @@
+const Quotes = [
+  {
+    author: "Arthur Schopenhauer",
+    quotation: "The two enemies of human happiness are pain and boredom."
+  },
+  {
+    author: "Guy Debord",
+    quotation:
+      "Behind the masks of total choice, different forms of the same alienation confront each other."
+  },
+  {
+    author: "Theodor Adorno",
+    quotation: "Art is magic delivered from the lie of being truth."
+  },
+  {
+    author: "Seneca",
+    quotation: "Fate leads the willing, and drags along the reluctant."
+  },
+  {
+    author: "Marcus Aurelius",
+    quotation:
+      "If you are pained by any external thing, it is not this thing that disturbs you, but your own judgment about it. And it is in your power to wipe out this judgment now."
+  },
+  {
+    author: "Epictetus",
+    quotation:
+      "We should always be asking ourselves: 'Is this something that is, or is not, in my control?'"
+  },
+  {
+    author: "Antonio Gramsci",
+    quotation:
+      "The challenge of modernity is to live without illusions and without becoming disillusioned."
+  },
+  {
+    author: "Michel Foucault",
+    quotation:
+      "There are forms of oppression and domination which become invisible - the new normal."
+  },
+  {
+    author: "Hannah Arendt",
+    quotation:
+      "In order to go on living one must try to escape the death involved in perfectionism."
+  },
+  {
+    author: "Walter Benjamin",
+    quotation:
+      "Memory is not an instrument for exploring the past but its theatre. It is the medium of past experience, as the ground is the medium in which dead cities lie interred."
+  },
+  {
+    author: "Albert Camus",
+    quotation:
+      "Real generosity toward the future lies in giving all to the present."
+  },
+  {
+    author: "Jacques Derrida",
+    quotation:
+      "I never give in to the temptation to be difficult just for the sake of being difficult. That would be too ridiculous."
+  }
+];
+
+export default Quotes;
