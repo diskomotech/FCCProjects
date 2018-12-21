748dbb6f7bfda46998d916d3efab0a83859de327
Jim Wright <jimwright808@gmail.com>
2018-12-21 13:16:00 +0400

    Basic CRA file set-up

---
+++ markdownPreviewer/react-version/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hellooooooo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
