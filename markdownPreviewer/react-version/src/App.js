feff8b066ebe0923ef9617a9379b00bc9d0e2991
Jim Wright <jimwright808@gmail.com>
2019-01-13 09:21:46 +0400

    Convert Editor into functional component. Move state into App

--- markdownPreviewer/react-version/src/App.js
+++ markdownPreviewer/react-version/src/App.js
@@ -4,10 +4,30 @@ import Editor from "./Editor";
 import Previewer from "./Previewer";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rawText:
+        "## This is a sub-heading... ### And here's some other cool stuff:"
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  textConversion = () => {
+    // Grab latest copy of state text
+
+    //Convert into markdown
+  }
+
   render() {
     return (
       <div className="App">
-        <Editor />
+        <Editor handleChange={this.handleChange} rawText={this.state.rawText} />
         <Previewer />
       </div>
     );
--- markdownPreviewer/react-version/src/Editor.js
+++ markdownPreviewer/react-version/src/Editor.js
@@ -1,37 +1,21 @@
-import React, { Component } from "react";
-import Previewer from "./Previewer";
+import React from "react";
 
-class Editor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rawText:
-        "## This is a sub-heading... ### And here's some other cool stuff:"
-    };
-  }
-
-  handleChange = event => {
-    this.setState({
-      text: event.target.value
-    });
-  };
-  render() {
-    return (
-      <div className="editor-container">
-        <form>
-          <label>
-            Editor
-            <input
-              type="textarea"
-              id="editor"
-              value={this.state.rawText}
-              onChange={this.handleChange}
-            />
-          </label>
-        </form>
-      </div>
-    );
-  }
-}
+const Editor = ({ rawText, handleChange }) => {
+  return (
+    <div className="editor-container">
+      <form>
+        <label>
+          Editor
+          <input
+            type="textarea"
+            id="editor"
+            value={rawText}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default Editor;
