a062d5f658bd65150d189fe761216482e99a77c7
Jim Wright <jimwright808@gmail.com>
2019-01-15 10:02:32 +0400

    Visual design finalised to match vanilla JS

--- localWeather/react-version/src/App.js
+++ localWeather/react-version/src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
+      <div>
+        <div className="contentBox">
+          <div className="headerTitle">
+            <h1>
+              FreeCodeCamp
+              <br />
+              Weather App
+            </h1>
+          </div>
+          <div className="weatherDisplay">
+            <button
+              type="button"
+              // className="btn btn-outline-secondary btn-lg"
+              id="locate"
+            >
+              Show Me The Weather
+            </button>
+            <div className="location" />
+            <div className="weather" />
+          </div>
+        </div>
       </div>
     );
   }
