99802348b5c367084185188a1357e2c90a304794
Jim Wright <jimwright808@gmail.com>
2019-01-16 11:16:57 +0400

    Attempt with existing soundplayer lib

--- drumMachine/react-version/src/DrumContainer.js
+++ drumMachine/react-version/src/DrumContainer.js
@@ -10,6 +10,40 @@ class DrumContainer extends Component {
       button: ""
     };
   }
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src =
+      "https://gustavgenberg.github.io/handy-front-end/SoundPlayer.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  playIt = action => {
+    // Initiate new instance of external sound player
+    const player = new SoundPlayer();
+
+    // Generate AWS URL for our sounds
+    const urlGenerator = (() =>
+      `https://s3.eu-west-2.amazonaws.com/diskomotech/freeCodeCamp/drumMachine/sounds/${action}.mp3`)();
+
+    // Play the sound
+    player.load(urlGenerator).play();
+
+    // Display sound name
+    // display.textContent = action;
+  };
+
+  animated = buttonTarget => {
+    // Add press effect animation
+    buttonTarget.classList.add("triggered");
+
+    // Remove press effect animation
+    setTimeout(() => {
+      buttonTarget.classList.remove("triggered");
+    }, 80);
+  };
+
   render() {
     return <DrumPresentational />;
   }
